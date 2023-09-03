7607eb590f98878cf44d2921820a3aa395a0fedd
Jaakko <jaakkopyrhonen@gmail.com>
2023-09-03 15:49:15 +0300

    Added response based on url. Refactored a bit

--- Coursework/NodeJS/index.js
+++ Coursework/NodeJS/index.js
@@ -4,24 +4,68 @@ const fs = require('fs');
 
 // check for the port. If the environment variable is not found, use 8080
 const PORT = process.env.PORT || 8080;
+const PUBLIC = 'public';
+
+const notFound = (res) => {
+    fs.readFile(path.join(__dirname, PUBLIC, '404.html'), (err, content) => {
+        res.writeHead(400, { 'Content-Type': 'html' });
+        res.end(content, 'utf-8');
+    });
+}
+
+const internalError = (res, err) => {
+    res.writeHead(500);
+    res.end(`Internal server error: ${err.code}`);
+}
+
+const handleErr = (res, err) => {
+    if (err.code == 'ENOENT') {
+        notFound(res);
+    } else {
+        internalError(res, err);
+    }
+}
+
+const returnContent = (res, contentType, content) => {
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(content, 'utf-8');
+}
+
+const getContentType = (filePath) => {
+    const extension = path.extname(filePath);
+    // default content type
+    let contentType = 'text/html';
+    switch (extension) {
+        case ".js":
+            contentType = "text/javascript";
+            break;
+        case ".css":
+            contentType = "text/css";
+            break;
+        case ".json":
+            contentType = "application/json";
+            break;
+        case ".png":
+            contentType = "image/png";
+            break;
+        case ".jpg":
+            contentType = "image/jpg";
+            break;
+    }
+    return contentType;
+}
 
 const server = http.createServer((req, res) => {
-    /*
-    if (req.url == '/') {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-            if (err) {
-                res.writeHead(500); 
-                res.end();
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'}); // write to the HTTP headers 
-            res.end(content);
-        });
-    }
-    else if (req.url == '/api/users') {
-        res.writeHead(200, {'Content-Type': 'application/json'}); // write to the HTTP headers 
-        res.end(JSON.stringify([{'Name': 'Bob'}, {'Name': 'Jane'}]));
-    }
-    */
+    const filePath = path.join(__dirname, PUBLIC, req.url == '/' ? 'index.html' : req.url);
+    const contentType = getContentType(filePath);
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            handleErr(res, err);
+        } else {
+            returnContent(res, contentType, content);
+        }
+    });
 });
 
 server.listen(PORT, () => console.log(`server running on ${PORT}`));
