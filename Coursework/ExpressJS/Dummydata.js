f98df7bbb2a0ef1d6dd1e1afdf075880224c8a8a
Jaakko <jaakkopyrhonen@gmail.com>
2023-09-04 15:41:29 +0300

    added dummy data for testing

---
+++ Coursework/ExpressJS/Dummydata.js
@@ -0,0 +1,14 @@
+const dummy = [
+    {
+        name: 'John Doe',
+        age: 30,
+        status: 'active'
+    },
+    {
+        name: 'Mary Sue',
+        age: 30,
+        status: 'inactive'
+    }
+]
+
+module.exports = dummy;
--- Coursework/ExpressJS/index.js
+++ Coursework/ExpressJS/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
+const dummydata = require('./Dummydata.js');
 
 const app = express();
 // set public as a static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/members', (req, res) => res.json(dummydata));
 
 app.get("/", (req, res) => {
     res.send("Hello World");
