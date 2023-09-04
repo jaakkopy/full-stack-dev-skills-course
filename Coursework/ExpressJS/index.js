d3020288825e83a8d41ade8d9ba22184fde5062f
Jaakko <jaakkopyrhonen@gmail.com>
2023-09-04 15:16:43 +0300

    Added a route for the root. Added nodemon as a dev dependency

--- Coursework/ExpressJS/index.js
+++ Coursework/ExpressJS/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express();
 
-
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
