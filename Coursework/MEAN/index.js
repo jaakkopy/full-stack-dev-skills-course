c4805bd6feb0a2a6d37fe2bfd59d7c7df6b7ddff
Jaakko <jaakkopyrhonen@gmail.com>
2023-09-07 14:59:51 +0300

    added database connection with mongoose

--- Coursework/MEAN/index.js
+++ Coursework/MEAN/index.js
@@ -3,12 +3,22 @@ const path = require('path');
 const cors = require('cors');
 const passport = require('passport-jwt');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to database ${config.database}`);
+});
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/users', require('./routes/users'));
+
+// static folder for the Angular build
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.send("Hello");
