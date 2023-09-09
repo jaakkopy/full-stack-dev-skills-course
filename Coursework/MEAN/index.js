fb5880fc3090558b99d7bc4df2c453d77316556f
Jaakko <jaakkopyrhonen@gmail.com>
2023-09-09 18:43:41 +0300

    added a catch-all route to the api for sending the built index.html

--- Coursework/MEAN/index.js
+++ Coursework/MEAN/index.js
@@ -30,6 +30,10 @@ app.get('/', (req, res) => {
     res.send("Hello");
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
