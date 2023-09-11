e402f8fde50d3c7bba8ee1e299df36c6d3c8d3bc
Jaakko <jaakkopyrhonen@gmail.com>
2023-09-11 13:04:39 +0300

    added route for creating a new group

--- Project/models/group.js
+++ Project/models/group.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const User = require('./user');
 const {hashPassword, comparePassword} = require('./authHelpers');
 
 const Groupchema = mongoose.Schema({
@@ -13,5 +14,25 @@ const Groupchema = mongoose.Schema({
     }
 });
 
+const validateNewGroupData = (newGroupData) => {
+    if (!newGroupData?.name || !newGroupData?.password)
+        return false;
+    if (newGroupData.name.length == 0 || newGroupData.password.length == 0)
+        return false;
+    return true;
+}
+
+const createGroup = async (creator, newGroupData) => {
+    if (!validateNewGroupData(newGroupData))
+        throw TypeError("Invalid group data");
+    newGroupData.password = await hashPassword(newGroupData.password);
+    const toAdd = Group(newGroupData);
+    await toAdd.save();
+    // add the creator as a member of the group
+    let user = await User.getUserById(creator.id);
+    user.groups.push(toAdd._id);
+    await user.save();
+}
 
 const Group = module.exports = mongoose.model('Group', Groupchema);
+module.exports.createGroup = createGroup;
--- Project/routes/groups.js
+++ Project/routes/groups.js
@@ -6,8 +6,25 @@ const helpers = require('./helpers.js');
 
 const router = Router();
 
-router.post('/create', passport.authenticate('jwt', {session: false}), (req, res) => {
-    res.send("hello");
+router.post('/create', passport.authenticate('jwt', {session: false}), async (req, res) => {
+    try {
+        await Group.createGroup(req.user, req.body);
+        res.status(200).json(helpers.successMsg("New group created"));
+    } catch (err) {
+        let status;
+        let msg;
+        if (err instanceof TypeError) {
+            status = 403;
+            msg = err.message;
+        } else if (err.message.indexOf('duplicate key error') !== -1) {
+            status = 403;
+            msg = "That group name is already taken";
+        } else {
+            status = 500;
+            msg = "Internal server error";
+        }
+        res.status(status).json(helpers.failureMsg(msg));
+    }
 });
 
 module.exports = router;
