b27a0e4c30ad900eb9ed68fa410259babf3472ce
Jaakko <jaakkopyrhonen@gmail.com>
2023-09-11 09:47:37 +0300

    formed connection to the database

--- Project/index.js
+++ Project/index.js
@@ -1,4 +1,17 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const dbConfig = require('./config/database');
+
+mongoose.connect(dbConfig.database);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to database ${dbConfig.database}`);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Database error: ${err}`);
+});
 
 const app = express();
 app.use(express.json());
