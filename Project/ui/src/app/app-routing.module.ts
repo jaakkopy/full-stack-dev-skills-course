d7b9114d60599b220a569d37faa50857eca9c9ef
Jaakko <jaakkopyrhonen@gmail.com>
2023-09-12 12:10:35 +0300

    added route guard and conditional rendering based on login status

--- Project/ui/src/app/app-routing.module.ts
+++ Project/ui/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthRouteGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', component: HomeComponent, canActivate: [AuthRouteGuard]},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
 ];
